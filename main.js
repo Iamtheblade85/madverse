d18e5043b980b5b8a7806bd0770b8f6cc7eee424
Iamtheblade85 <141064917+Iamtheblade85@users.noreply.github.com>
2025-04-29 20:47:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ function loadSection(section) {
       <div id="pagination" class="flex justify-center items-center space-x-4 mt-6"></div>
   
       <div id="modal-nft" class="fixed inset-0 hidden bg-black bg-opacity-50 flex items-center justify-center">
-        <div class="bg-white p-6 rounded shadow max-w-md w-full relative">
+        <div class="bg-white p-6 rounded shadow max-w-md w-full relative" style="max-height: 80vh; overflow-y: auto;">
           <button id="close-modal" class="absolute top-2 right-2 text-gray-400 hover:text-black">&times;</button>
           <div id="modal-content"></div>
         </div>

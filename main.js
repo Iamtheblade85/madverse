10529a719f0147ca92e624639baa21a29781273c
Iamtheblade85 <141064917+Iamtheblade85@users.noreply.github.com>
2025-04-30 07:38:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -151,7 +151,7 @@ function loadSection(section) {
   
       <div id="modal-nft" class="fixed inset-0 hidden bg-black bg-opacity-50 flex items-center justify-center">
         <div class="bg-white p-6 rounded shadow max-w-md w-full relative" style="max-height: 80vh; overflow-y: auto;">
-          <button id="close-modal" class="absolute top-2 right-2 text-gray-400 hover:text-black">&times;</button>
+          <button id="close-modal" class="absolute top-2 right-2 text-red-600 hover:text-red-800 text-4xl font-bold">&times;</button>
           <div id="modal-content"></div>
         </div>
       </div>
